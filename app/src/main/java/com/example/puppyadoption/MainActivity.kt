d170d77e3402de4223e32c18fbd84beabd571663
fr1x3 <gathufx@gmail.com>
2021-03-08 12:25:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/puppyadoption/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.puppyadoption
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.puppyadoption.ui.theme.PuppyAdoptionTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PuppyAdoptionTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun MyApp(){
+    PuppyAdoptionTheme{
+
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PuppyAdoptionTheme {
+        Greeting("Android")
+    }
+}
